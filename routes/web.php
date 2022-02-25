853c23a5db2c8ec6dec22af7ad5bc9df5ebd022b
Yudas <yudasmalabi@gmail.com>
2022-02-25 17:36:40 +0700

    done ProductsPage

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::prefix('category')->group(function () {
+    Route::get('marbel-edu-games', [ProductController::class, 'eduGames']);
+    Route::get('marbel-and-friends-kids-games', [ProductController::class, 'kidsGames']);
+    Route::get('riri-story-books', [ProductController::class, 'storyBooks']);
+    Route::get('kolak-kids-songs', [ProductController::class, 'kidsSongs']);
+});
